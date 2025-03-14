dcbdfdd546dfc0765e2b8006220d89455a7824ff
pcwd <41215821+pcwd@users.noreply.github.com>
2025-03-14 16:32:50 +1000

    Update actions.ts
    line 77

--- app/actions.ts
+++ app/actions.ts
@@ -74,7 +74,7 @@ export async function createWallet(
       method: "POST",
       body: JSON.stringify({
         name:
-          APP_NAME_PREFIX + Math.floor(Date.now() / 1000) + request?.username,
+          APP_NAME_PREFIX + Math.floor(Date.now() / 1000) + "-" + ${params.username},
         pubkey: "",
         budgetRenewal: "monthly",
         maxAmount: 0,
